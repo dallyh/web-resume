cb0e9a8188230ca41e21b09f49543c325bf33c0b
Dalibor Hon <hon.dalibor@gmail.com>
2023-01-25 22:44:24 +0100

    Add astro-compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://resume.daliborhon.com",
-  integrations: [astroI18next(), sitemap(), robotsTxt()]
+  integrations: [astroI18next(), sitemap(), robotsTxt(), compress()]
 });
