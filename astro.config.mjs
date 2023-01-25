50a7461fee57afcf486a1731b70d21bf0a39f04f
Dalibor Hon <hon.dalibor@gmail.com>
2023-01-25 21:49:02 +0100

    Add sitemap and robots.txt integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [astroI18next()]
+  site: "https://resume.daliborhon.com",
+  integrations: [astroI18next(), sitemap(), robotsTxt()]
 });
