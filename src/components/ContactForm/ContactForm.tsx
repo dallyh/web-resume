8181d1943b0d0359944e72fd9eb349eefe94750e
Dalibor Hon <hon.dalibor@gmail.com>
2023-05-14 14:43:08 +0200

    Add some more form animations

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -12,7 +12,9 @@ const contactForm = () => {
     const { t } = useTranslation("contactForm");
     const isFirstRender = useRef(true);
     const formRef = useRef(null);
+    const formWrapperRef = useRef(null);
 
+    // Handle submit and errors
     useEffect(() => {
         if (isFirstRender.current) {
             isFirstRender.current = false;
@@ -21,10 +23,13 @@ const contactForm = () => {
         validateError(state.errors);
     }, [state.errors]);
 
+    // Animate form
     useEffect(() => {
         formRef.current && autoAnimate(formRef.current);
-    }, [formRef]);
+        formWrapperRef.current && autoAnimate(formWrapperRef.current);
+    }, [formRef, formWrapperRef]);
 
+    // Validate form submission errors
     const validateError = (errors: FormError[]) => {
         if (errors.length === 0) {
             console.log("0");
@@ -63,30 +68,34 @@ const contactForm = () => {
             }
         });
     };
-
-    if (state.succeeded) {
-        return <p>{t("contactForm:SubmitSuccess")}</p>;
-    }
-
     return (
-        <form id="fs-frm" ref={formRef} name="simple-contact-form" onSubmit={handleSubmit}>
-            <fieldset id="fs-frm-inputs">
-                <label htmlFor="full-name">{t("contactForm:FullName")}</label>
-                <input type="text" name="name" id="full-name" placeholder={t("contactForm:NameAndSurname") as string} required />
-                <label htmlFor="email">{t("contactForm:EmailAddress")}</label>
-                <input id="email" type="email" name="email" placeholder="@" required />
-                {errorState.some((key) => key.type === "EMAIL") && <p className="error">{errorState.find((item) => item.type === "EMAIL")?.message}</p>}
-                <label htmlFor="message">{t("contactForm:Message")}</label>
-                <textarea rows={5} name="message" id="message" placeholder={t("contactForm:MessagePlaceholder") as string} required></textarea>
-                {errorState.some((key) => key.type === "TEXT") && <p className="error">{errorState.find((item) => item.type === "EMAIL")?.message}</p>}
-                <input type="hidden" name="_subject" id="email-subject" value="New resume submission from {{ name }}" />
-                <input type="hidden" name="_language" value="cs" />
-            </fieldset>
-            {errorState.some((key) => key.type != "") && <p className="error">{t("contactForm:ERROR_CORRECT_FIELDS")}</p>}
-            <button type="submit" id="fs-frm-submit-button" className="button" disabled={state.submitting}>
-                {t("contactForm:Submit")}
-            </button>
-        </form>
+        <div ref={formWrapperRef}>
+            {state.succeeded && (
+                <div id="submit-success">
+                    <p>{t("contactForm:SubmitSuccess")}</p>
+                </div>
+            )}
+            {!state.succeeded && (
+                <form id="fs-frm" ref={formRef} name="simple-contact-form" onSubmit={handleSubmit}>
+                    <fieldset id="fs-frm-inputs">
+                        <label htmlFor="full-name">{t("contactForm:FullName")}</label>
+                        <input type="text" name="name" id="full-name" placeholder={t("contactForm:NameAndSurname") as string} required />
+                        <label htmlFor="email">{t("contactForm:EmailAddress")}</label>
+                        <input id="email" type="email" name="email" placeholder="@" required />
+                        {errorState.some((key) => key.type === "EMAIL") && <p className="error">{errorState.find((item) => item.type === "EMAIL")?.message}</p>}
+                        <label htmlFor="message">{t("contactForm:Message")}</label>
+                        <textarea rows={5} name="message" id="message" placeholder={t("contactForm:MessagePlaceholder") as string} required></textarea>
+                        {errorState.some((key) => key.type === "TEXT") && <p className="error">{errorState.find((item) => item.type === "EMAIL")?.message}</p>}
+                        <input type="hidden" name="_subject" id="email-subject" value="New resume submission" />
+                        <input type="hidden" name="_language" value="cs" />
+                    </fieldset>
+                    {errorState.some((key) => key.type != "") && <p className="error">{t("contactForm:ERROR_CORRECT_FIELDS")}</p>}
+                    <button type="submit" id="fs-frm-submit-button" className="button" disabled={state.submitting}>
+                        {t("contactForm:Submit")}
+                    </button>
+                </form>
+            )}
+        </div>
     );
 };
 
